2742b6096dd4c287787604b6d7deb27b43b2bc1e
Roger Steve Ruiz <hi@rog.gr>
2017-02-14 23:53:42 -0500

    It's just glue code around the ledger-cli

--- index.js
+++ index.js
@@ -3,13 +3,15 @@ const RtmClient = require( '@slack/client' ).RtmClient
 const RTM_CLIENT_EVENTS = require( '@slack/client' ).CLIENT_EVENTS.RTM
 const token = process.env.SLACK_API_TOKEN
 const channelId = process.env.SLACK_CHANNEL_ID
-const rtm = new RtmClient( token, { logLevel: 'debug' } )
+const rtm = new RtmClient( token )
+const dollars = require( './lib/get-money' )
 
 rtm.start()
-
 rtm.on( RTM_CLIENT_EVENTS.RTM_CONNECTION_OPENED, function () {
-  rtm.sendMessage( 'Buenos dias', channelId, function () {
-    console.log( 'message sent, shutting down @el_capo' )
-    process.exit( 0 )
+  dollars.balance( function ( message ) {
+    rtm.sendMessage( message, channelId, function() {
+      console.log( 'message sent, shutting down @el_capo' )
+      process.exit( 0 )
+    } )
   } )
 } )
